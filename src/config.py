530f20b1781fd8afc5d4efb37a7cfba0934238da
laukendra <laukendras@gmail.com>
2024-06-05 14:18:07 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 LOG_LEVEL = os.environ["LOG_LEVEL"]
 LLM_VERBOSE = os.environ["LLM_VERBOSE"]
-LOG_FILE_PATH = os.environ["LOG_FILE_PATH"]
+#LOG_FILE_PATH = os.environ["LOG_FILE_PATH"]
 INDEX_NAME = os.environ["INDEX_NAME"]
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV=os.getenv('PINECONE_ENV')
@@ -15,7 +15,7 @@ log.basicConfig(
     level=LOG_LEVEL,
     format='%(asctime)s - %(levelname)s : %(filename)s #%(lineno)d - %(message)s',
     handlers=[
-        log.FileHandler(LOG_FILE_PATH),
+        #log.FileHandler(LOG_FILE_PATH),
         log.StreamHandler()
     ]
 )
